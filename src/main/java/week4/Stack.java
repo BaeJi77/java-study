2ebf2dc8fb81c0c84b1f6baf49f0c924b6d15e59
baeji.hoon <baeji.hoon@linecorp.com>
2020-12-06 20:39:41 +0900

    Implement Stack

---
+++ src/main/java/week4/Stack.java
@@ -0,0 +1,34 @@
+package week4;
+
+/**
+ * int 배열을 사용해서 정수를 저장하는 Stack을 구현하세요.
+ * void push(int data)를 구현하세요.
+ * int pop()을 구현하세요.
+ */
+
+public class Stack {
+
+    private int[] arr;
+    private int size;
+
+    public Stack() {
+        this.arr = new int[1_000_000_000];
+        this.size = 0;
+    }
+
+    public void push(int number) {
+        arr[size] = number;
+        size++;
+    }
+
+    public int pop() {
+        if (size <= 0) {
+            throw new DataStructureException("stack is empty");
+        }
+        return arr[--size];
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+}
